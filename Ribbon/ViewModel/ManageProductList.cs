60ddc94123db2b5b96cf6038eeb2589a050d279f
Alamgir Kabir <alamgirkabir7@gmail.com>
2015-11-17 01:52:37 +0600

    add grid view with list

--- Ribbon/ViewModel/ManageProductList.cs
+++ Ribbon/ViewModel/ManageProductList.cs
@@ -2,16 +2,38 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-
+using com.inventory.bean;
+using com.inventory.db.repositories;
+using com.inventory.db;
+using java.util;
 namespace Ribbon.ViewModel
 {
     class ManageProductList : BindableBase
     {
+        ObservableCollection<ProductInfo> _productList;
+
+        public ObservableCollection<ProductInfo> ProductList
+        { 
+            get {
+                ProductManager productManager = new ProductManager();
+
+                _productList = new ObservableCollection<ProductInfo>();
+                for (Iterator i = productManager.getAllProducts().iterator(); i.hasNext(); ){
+                    _productList.Add((ProductInfo)i.next());
+                }
+
+                return _productList;
+            }
+            set {
+                this._productList = value;
+            }
+        }
     }
 }
