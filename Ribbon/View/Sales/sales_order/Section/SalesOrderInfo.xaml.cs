834646a4372d85965f4d527ac77b3dd497330313
Alamgir Kabir <alamgirkabir7@gmail.com>
2015-11-18 17:22:31 +0600

    incomplete push on request of redooy

--- Ribbon/View/Sales/sales_order/Section/SalesOrderInfo.xaml.cs
+++ Ribbon/View/Sales/sales_order/Section/SalesOrderInfo.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,20 @@ namespace Ribbon.View
         public SalesOrderInfo()
         {
             InitializeComponent();
+        }
+
+        private void productSaleOrderGrid_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            DataGrid grid = (DataGrid) sender;
+            Popup productSelector = (Popup)grid.FindName("productSelector");
+            //productSelector.PlacementTarget = grid.GetCell(e.Row.GetIndex(), e.Column.DisplayIndex);
+            productSelector.IsOpen = true;
+            
+        }
+
+        private void productSaleOrderGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
         }
     }
 }
